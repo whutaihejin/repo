edbc1c9a8a84ab184965ec313d6b315809eabda4
zhanglong <zhanglonghanks@didichuxing.com>
2017-07-30 17:18:21 +0800

    forward

--- src/l523.cc
+++ src/l523.cc
@@ -1,21 +1,23 @@
 #include <iostream>
 #include <vector>
+#include <map>
 #include "gtest/gtest.h"
 
 using namespace std;
 
 namespace {
      bool checkSubarraySum(vector<int>& nums, int k) {
-         int size = nums.size();
-         for (int low = 0; low < size; ++low) {
-             int sum = nums[low];
-             for (int high = low + 1; high < size; ++high) {
-                 sum += nums[high];
-                 if (k == 0) {
-                     if (sum == 0) return true;
-                 } else if (sum % k == 0) {
-                     return true;
-                 }
+         int sum = 0, size = nums.size();
+         std::map<int, int> hash;
+         std::map<int, int>::iterator it;
+         hash.insert(std::make_pair(0, -1));
+         for (int i = 0; i < size; ++i) {
+             sum += nums[i];
+             if (k != 0) sum %= k;
+             if ((it = hash.find(sum)) != hash.end()) {
+                 if (i - it->second >= 2) return true;
+             } else {
+                 hash.insert(std::make_pair(sum, i));
              }
          }
          return false;
@@ -32,6 +34,7 @@ TEST(CheckSubarraySumTest, Case1) {
     int array[] = {23, 2, 6, 4, 7};
     std::vector<int> vec(&array[0], &array[4]);
     ASSERT_TRUE(checkSubarraySum(vec, 6)); 
+    ASSERT_FALSE(checkSubarraySum(vec, 0)); 
 }
 
 TEST(CheckSubarraySumTest, Case2) {
@@ -45,6 +48,24 @@ TEST(CheckSubarraySumTest, Case3) {
     int array[] = {23, 2, 6, 4, 7};
     std::vector<int> vec(&array[0], &array[4]);
     ASSERT_FALSE(checkSubarraySum(vec, 0)); 
+}
+
+TEST(CheckSubarraySumTest, Case4) {
+    int array[] = {1,2,0,0,0,0,8};
+    std::vector<int> vec(&array[0], &array[6]);
+    ASSERT_TRUE(checkSubarraySum(vec, 6)); 
+}
+
+TEST(CheckSubarraySumTest, Case5) {
+    int array[] = {0,0,0,0,0,0,0};
+    std::vector<int> vec(&array[0], &array[6]);
+    ASSERT_TRUE(checkSubarraySum(vec, 6)); 
+}
+
+TEST(CheckSubarraySumTest, Case6) {
+    int array[] = {1,0,1,0,0,0,0};
+    std::vector<int> vec(&array[0], &array[6]);
+    ASSERT_TRUE(checkSubarraySum(vec, 6)); 
 }
 
 int main(int argc, char* argv[]) {
