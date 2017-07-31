225c8168a1029e202983e39827dc54e86b46464e
zhanglong <zhanglonghanks@didichuxing.com>
2017-08-01 00:24:06 +0800

    clrs forward

---
+++ src/a22.1.3.cc
@@ -0,0 +1,55 @@
+#include <iostream>
+#include "gtest/gtest.h"
+
+using namespace std;
+
+#define COLUMN 4
+
+namespace {
+    // tranpose matrix
+    bool Transpose(int (*graph)[COLUMN], int size) {
+        if (size != COLUMN) return false;
+        for (int i = 0; i < size; ++i) {
+            for (int j = i + 1; j < COLUMN; ++j) {
+                int val = graph[i][j];
+                graph[i][j] = graph[j][i];
+                graph[j][i] = val;
+            }
+        }
+        return true;
+    }
+    
+    // check graph eaual
+    bool Equal(int (*graph)[COLUMN], int (*epect)[COLUMN], int size) {
+        if (size != COLUMN) return false;
+        for (int i = 0; i < size; ++i) {
+            for (int j = 0; j < COLUMN; ++j) {
+                if (graph[i][j] != epect[i][j]) return false;
+            }
+        }
+        return true;
+    }
+    
+} // anonymous namespace
+
+TEST(TransposeTest, Case0) {
+    int graph[4][4] = {
+        {0, 1, 0, 0},
+        {0, 0, 0, 0},
+        {1, 1, 0, 1},
+        {0, 1, 0, 0}
+    };
+    int expect[4][4] = {
+        {0, 0, 1, 0},
+        {1, 0, 1, 1},
+        {0, 0, 0, 0},
+        {0, 0, 1, 0}
+    };
+    Transpose(graph, COLUMN);
+    ASSERT_TRUE(Equal(graph, expect, COLUMN)); 
+}
+
+int main(int argc, char* argv[]) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
