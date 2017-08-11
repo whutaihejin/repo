5d4e82f58305126eddecc2bd654191c4def7e7de
whutaihejin <taihejin@163.com>
2017-08-11 17:57:11 +0800

    forward

--- src/bench.cc
+++ src/bench.cc
@@ -1,13 +1,39 @@
 #include <iostream>
+#include <vector>
+#include <map>
 #include "benchmark/benchmark.h"
 
 using namespace std;
 
+namespace {
+    vector<int> twoSum(vector<int>& nums, int target) {
+        std::vector<int> index_list;
+        std::map<int, int> hash;
+        std::map<int, int>::iterator it;
+        int size = nums.size();
+        for (int i = 0; i < size; ++i) {
+            int key = nums[i];
+            if ((it = hash.find(key)) != hash.end()) {
+                index_list.push_back(it->second);
+                index_list.push_back(i);
+                break;
+            } else {
+                hash.insert(std::make_pair(target - key, i));
+            }
+        }
+        return index_list;
+    }
+} // anonymous namespace
+
 // Define another benchmark
 static void BM_StringCopy(benchmark::State& state) {
-    std::string x = "hello";
+    vector<int> nums;
+    nums.push_back(2);
+    nums.push_back(7);
+    nums.push_back(11);
+    nums.push_back(15);
     while (state.KeepRunning()) {
-        std::string copy(x);
+        vector<int> index_list = twoSum(nums, 9);
     }
 }
 
