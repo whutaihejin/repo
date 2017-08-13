2c97b0ae75b2e9bc5c64ff1f92f2177adf8d23c9
whutaihejin <taihejin@163.com>
2017-08-13 12:54:31 +0800

    fighting

---
+++ src/l191.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include "gtest/gtest.h"
+
+using namespace std;
+
+namespace {
+    int hammingWeight(uint32_t n) {
+        int weight = 0;
+        while (n != 0) {
+            weight += ((n & 0x01) == 0 ? 0 : 1);
+            n >>= 1;
+        }
+        return weight;          
+    }
+} // anonymous namespace
+
+TEST(hammingWeightTest, Case0) {
+    ASSERT_EQ(0, hammingWeight(0U)); 
+    ASSERT_EQ(1, hammingWeight(1U)); 
+    ASSERT_EQ(1, hammingWeight(2U)); 
+    ASSERT_EQ(3, hammingWeight(11U)); 
+    ASSERT_EQ(4, hammingWeight(0x0FU)); 
+}
+
+int main(int argc, char* argv[]) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
