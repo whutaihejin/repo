99037b395066ea86a0603d98e123f3c94c3d0e81
whutaihejin <taihejin@163.com>
2017-08-23 16:03:05 +0800

    format code

---
+++ src/apue/t15.5.cc
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#define MAXLINE 1024
+
+int main() {
+    int fd[2];
+    pid_t pid;
+    char line[MAXLINE];
+    if (pipe(fd) < 0) {
+        printf("pipe error\n"), exit(1);
+    }
+    if ((pid = fork()) < 0) {
+        printf("fork error\n"), exit(2);
+    } else if (pid > 0) { // parent
+        close(fd[0]);
+        write(fd[1], "hello world\n", 12);
+    } else { // child
+        close(fd[1]);
+        int n = read(fd[0], line, MAXLINE);
+        write(STDOUT_FILENO, line, n);
+    }
+    return 0;
+}
