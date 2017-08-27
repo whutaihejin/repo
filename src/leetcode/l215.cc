d15f27a67cf658ce55274807ce60af67f920c896
whutaihejin <taihejin@163.com>
2017-08-27 18:03:35 +0800

    forward

--- src/leetcode/l215.cc
+++ src/leetcode/l215.cc
@@ -6,43 +6,44 @@
 using namespace std;
 
 namespace {
-  void Heapify(int arr[], int size, int index) {
+  void Heapify(std::vector<int>& heap, int index) {
+    int size = heap.size();
     while (index < size) {
       int left = (index << 1) + 1;
       int right = left + 1;
       int smallest = index;
-      if (left < size && arr[left] < arr[index]) {
+      if (left < size && heap[left] < heap[index]) {
         smallest = left;
       }
-      if (right < size && arr[right] < arr[smallest]) {
+      if (right < size && heap[right] < heap[smallest]) {
         smallest = right;
       }
       if (smallest == index) break;
       // exchange index <-> smallest
-      int val = arr[index];
-      arr[index] = arr[smallest];
-      arr[smallest] = val;
+      int val = heap[index];
+      heap[index] = heap[smallest];
+      heap[smallest] = val;
       index = smallest;
     }
   }
 
-  void BuildHeap(int arr[], int size) {
-    for (int i = size / 2 - 1; i >= 0; --i) {
-      Heapify(arr, size, i);
+  void BuildHeap(std::vector<int>& heap) {
+    for (int i = heap.size() / 2 - 1; i >= 0; --i) {
+      Heapify(heap, i);
     }
   }
   
   int findKthLargest(vector<int>& nums, int k) {
-    int* heap = new int[k];
-    size_t i = 0;
+    std::vector<int> heap(k);
+    int i = 0;
     for (i = 0; i < k; ++i) {
       heap[i] = nums[i];
     }
-    BuildHeap(heap, k);
-    for (; i < nums.size(); ++i) {
+    BuildHeap(heap);
+    for (; i < (int)nums.size(); ++i) {
       if (nums[i] > heap[0]) {
         heap[0] = nums[i];
-        Heapify(heap, k, 0);
+        Heapify(heap,  0);
       }
     }
     return heap[0];
@@ -50,13 +51,14 @@ namespace {
 } // anonymous namespace
 
 TEST(findKthLargestTest, Case0) {
-    int arr[] = {1, 2, 3, 4, 5, 6, 7};
-    Heapify(arr, sizeof(arr) / sizeof(int), 0);
-}
-
-TEST(findKthLargestTest, Case1) {
   std::vector<int> vec = {1, 2, 3, 4, 5, 4, 5};
+    ASSERT_EQ(5, findKthLargest(vec, 1)); 
+    ASSERT_EQ(5, findKthLargest(vec, 2)); 
     ASSERT_EQ(4, findKthLargest(vec, 3)); 
+    ASSERT_EQ(4, findKthLargest(vec, 4)); 
+    ASSERT_EQ(3, findKthLargest(vec, 5)); 
+    ASSERT_EQ(2, findKthLargest(vec, 6)); 
+    ASSERT_EQ(1, findKthLargest(vec, 7)); 
 }
 
 int main(int argc, char* argv[]) {
