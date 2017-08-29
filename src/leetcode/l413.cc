84d1fe1a0dac5841a0fcccc9986f16d282566a8f
whutaihejin <taihejin@163.com>
2017-08-29 16:11:09 +0800

    forward

--- src/leetcode/l413.cc
+++ src/leetcode/l413.cc
@@ -17,8 +17,8 @@ namespace {
         for (size_t len = 3; len <= A.size(); ++len) {
             for (size_t i = 0; i <= A.size() - len; ++i) {
                 int j = i + len - 1;
-                if (matrix[i][j - 1] && (A[i] + A[j - 1] == (A[j] << 1))) {
-                    matrix[i][len + i - 1] = 1;
+                if (matrix[i][j - 1] && (A[j - 2] + A[j] == (A[j - 1] << 1))) {
+                    matrix[i][j] = 1;
                     count++;
                 }
             }
@@ -32,6 +32,10 @@ TEST(YourTest, Case0) {
     ASSERT_EQ(3, numberOfArithmeticSlices(nums)); 
 }
 
+TEST(YourTest, Case1) {
+    vector<int> nums = {1, 1, 2, 5, 7};
+    ASSERT_EQ(0, numberOfArithmeticSlices(nums)); 
+}
 int main(int argc, char* argv[]) {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
