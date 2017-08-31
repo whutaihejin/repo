9db0feba14e9cb47f29e025488a6a1fe5f5dfbbb
whutaihejin <taihejin@163.com>
2017-08-31 15:22:19 +0800

    forward

--- src/apue/t16.3.2.cc
+++ src/apue/t16.3.2.cc
@@ -3,9 +3,12 @@
 #include <sys/socket.h>
 
 int main() {
+  // str to net addr
   int32_t addr;
   const char* str = "10.21.188.12";
-  int ret = inet_pton(AF_INET, str, &addr);
+  inet_pton(AF_INET, str, &addr);
+
+  // net addr to str
   char buf[INET_ADDRSTRLEN];
   const char* s = inet_ntop(AF_INET, &addr, buf, INET_ADDRSTRLEN);
   printf("raw %s --> %u --> %s --> %s\n", str, addr, buf, s);
