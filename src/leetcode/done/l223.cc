9d5a292ae91e4f65fb0bd807fde4d4f841bc682b
whutaihejin <taihejin@163.com>
2017-09-18 20:36:19 +0800

    forward

--- src/leetcode/done/l223.cc
+++ src/leetcode/done/l223.cc
@@ -8,7 +8,7 @@ namespace {
     int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
         int area_x = (C - A) * (D - B);
         int area_y = (G - E) * (H - F);
-        if (G <= A || E >= C) { // no intersection
+        if (G <= A || E >= C || H <= B || F >= D) { // no intersection
             return area_x + area_y;
         }
         int lx = 0, hx = 0;
@@ -16,9 +16,6 @@ namespace {
             lx = A, hx = std::min(G, C);
         } else { // right intersection
             lx = E, hx = std::min(G, C);
-        }
-        if (H <= B || F >= D) { // no intersection
-            return area_x + area_y;
         }
         int ly = 0, hy = 0;
         if (F <= B && H >= B) { // bottom intersection
