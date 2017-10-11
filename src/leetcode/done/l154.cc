8cf41701e478a8d5e69f68fd888e8f01004efa44
whutaihejin <taihejin@163.com>
2017-10-11 16:48:13 +0800

    more intelligent code

--- src/leetcode/done/l154.cc
+++ src/leetcode/done/l154.cc
@@ -1,44 +1,22 @@
 #include <iostream>
-
 #include <gtest/gtest.h>
 
 using namespace std;
 
 namespace {
-    int linearMin(vector<int>& nums) {
-        int min_val = nums[0];
-        for (size_t i = 1; i < nums.size(); ++i) {
-           min_val = std::min(min_val, nums[i]); 
-        }
-        return min_val;
-    }
     int findMin(vector<int>& nums) {
         int low = 0, high = nums.size() - 1;
-        while (low + 1 < high) {
+        while (low < high) {
             int mid = low + (high - low) / 2;
-            if (nums[low] < nums[mid]) {
-                if (nums[mid] <= nums[high]) {
-                    high = mid;
-                } else {
-                    low = mid;
-                }
-            } else if (nums[mid] == nums[high]) {
-                if (nums[mid] < nums[high]) {
-                    high = mid;
-                } else if (nums[mid] == nums[high]) {
-                    return linearMin(nums);
-                } else {
-                    low = mid;
-                }
+            if (nums[mid] < nums[high]) {
+                high = mid;
+            } else if (nums[mid] > nums[high]) {
+                low = mid + 1;
             } else {
-                if (nums[mid] <= nums[high]) {
-                    high = mid;
-                } else { 
-                    low = mid;
-                }
+                high--;
             }
         }
-        return nums[low] < nums[high] ? nums[low] : nums[high];
+        return nums[low];
     }
 } // anonymous namespace
 
