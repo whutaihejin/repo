da869525c5535c7ce8cd9cfb7020638ee833dca6
whutaihejin <taihejin@163.com>
2017-10-27 23:58:31 +0800

    not flush if coredump

--- src/apue/t5.9.cc
+++ src/apue/t5.9.cc
@@ -5,6 +5,11 @@ int main() {
     FILE* file_ = fopen("info.log", "ab");
     std::string line(512, 'a');
     fwrite(&line[0], 1, line.size(), file_);
+    // fflush(file_);
+    int* less = NULL;
+    if (*less == 10) {
+        printf("just mock it!\n");
+    }
     std::string next(512, 'b');
     fwrite(&next[0], 1, next.size(), file_);
     return 0;
