36090193409f564ac6b059303dc73ce5a178c57b
whutaihejin <taihejin@163.com>
2017-11-23 19:48:45 +0800

    forward

--- src/csapp/detect.c
+++ src/csapp/detect.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 int main() {
-  printf("char=%dbyte\n", sizeof(char));
-  printf("short=%dbyte\n", sizeof(short));
-  printf("int=%dbyte\n", sizeof(int));
-  printf("long int=%dbyte\n", sizeof(long int));
-  printf("long long int=%dbyte\n", sizeof(long long int));
-  printf("char*=%dbyte\n", sizeof(char*));
-  printf("float=%dbyte\n", sizeof(float));
-  printf("double=%dbyte\n", sizeof(double));
-  printf("long double=%dbyte\n", sizeof(long double));
+  printf("char=%lubyte\n", sizeof(char));
+  printf("short=%lubyte\n", sizeof(short));
+  printf("int=%lubyte\n", sizeof(int));
+  printf("long int=%lubyte\n", sizeof(long int));
+  printf("long long int=%lubyte\n", sizeof(long long int));
+  printf("char*=%lubyte\n", sizeof(char*));
+  printf("float=%lubyte\n", sizeof(float));
+  printf("double=%lubyte\n", sizeof(double));
+  printf("long double=%lubyte\n", sizeof(long double));
   return 0;
 }
