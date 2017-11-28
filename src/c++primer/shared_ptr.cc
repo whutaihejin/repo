1207ef175cf629864d126d4e106aeac5fe0e8163
zhanglonghanks <zhanglonghanks@didichuxing.com>
2017-11-28 10:22:51 +0800

    forward

--- src/c++primer/shared_ptr.cc
+++ src/c++primer/shared_ptr.cc
@@ -18,5 +18,14 @@ int main() {
     if (px && !px->empty()) {
         std::cout << "px=" << *px << std::endl;
     }
+    //
+    std::shared_ptr<int> p3 = std::make_shared<int>(11);
+    std::cout << "p3=" << p3 << " *p3=" << *p3 << std::endl;
+    //
+    std::shared_ptr<std::string> p4 = std::make_shared<std::string>(10, '9');
+    std::cout << "p4=" << p4 << " *p4=" << *p4 << std::endl;
+    //
+    std::shared_ptr<int> p5 = std::make_shared<int>();
+    std::cout << "p5=" << p5 << " *p5=" << *p5 << std::endl;
     return 0;
 }
