77db79360380248a4e3f1ce9746ed3b20922c39a
whutaihejin <taihejin@163.com>
2017-12-06 10:08:07 +0800

    forward

---
+++ src/c++primer/chapter1/e1.7.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    // /* wrong comment
+    //  * nested comments /**/ // warning: '/*' within block comment [-Wcomment]
+    //  */ // error: expected expression
+    // return 0; // error: expected expression
+    return 0;
+}
