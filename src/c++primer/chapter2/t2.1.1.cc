1a94ec611a866430a8d53b6a3d61c4c369e42013
whutaihejin <taihejin@163.com>
2017-12-06 20:32:20 +0800

    forward

---
+++ src/c++primer/chapter2/t2.1.1.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+
+#define size(x) sizeof(x)
+#define str(x) "sizeof("#x") = "
+#define print_size(x) std::cout << str(x) << size(x) << std::endl;
+
+int main() {
+    print_size(bool);
+    print_size(char);
+    print_size(wchar_t);
+    print_size(char16_t);
+    print_size(char32_t);
+    print_size(short);
+    print_size(int);
+    print_size(long);
+    print_size(long long);
+    print_size(float);
+    print_size(double);
+    print_size(long double);
+    
+   
+    // int32_t
+    print_size(int32_t);
+    print_size(int64_t);
+    return 0;
+}
