978a09ce0371126614a4da4b5d413a9e80012977
whutaihejin <taihejin@163.com>
2017-12-06 19:02:30 +0800

    forward

--- src/c++primer/chapter1/t1.5.1.cc
+++ src/c++primer/chapter1/t1.5.1.cc
@@ -30,6 +30,14 @@ friend std::ostream& operator<<(std::ostream& out, const SalesItem& item) {
     return out;
 }
 
+friend SalesItem& operator+(SalesItem& x, SalesItem& y) {
+    if (x.Isbn() == y.Isbn()) {
+        x.sold_number_ += y.sold_number_;
+        x.total_revenue_ += y.total_revenue_;
+    }
+    return x;
+}
+
 private:
     std::string isbn_;
     // total revenue
@@ -37,12 +45,16 @@ private:
     // number of copies sold
     int sold_number_;
 };
+
 int main() {
 
-    SalesItem book;
+    SalesItem book, primer;
     std::cin >> book;
+    // std::cout << book << std::endl;
 
-    std::cout << book << std::endl;
+    std::cin >> primer;
+
+    std::cout << book + primer << std::endl;
 
     return 0;
 }
