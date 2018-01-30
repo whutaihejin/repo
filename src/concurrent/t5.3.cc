886192c7bcd42e5ad5b3f42d9138a5327e4c32af
zhanglonghanks <zhanglonghanks@didichuxing.com>
2018-01-30 20:58:07 +0800

    forward

--- src/concurrent/t5.3.cc
+++ src/concurrent/t5.3.cc
@@ -2,13 +2,16 @@
 #include <atomic>
 #include <thread>
 #include <vector>
+#include <chrono>
 
 std::vector<int> data;
 std::atomic<bool> data_ready(false);
 
 void ReadThread() {
     while (!data_ready.load()) {
-        // std::this_thread::sleep(std::milliseconds(1));
+      std::cout << "test --begin" << std::endl;
+      std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+      std::cout << "test --end" << std::endl;
     }
     std::cout << "The answer = " << data[0] << std::endl;
 }
