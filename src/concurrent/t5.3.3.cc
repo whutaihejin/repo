474f39de7930f660b3682a9bb426ff4c95cc600c
whutaihejin <taihejin@163.com>
2018-01-31 10:20:31 +0800

    fighting

---
+++ src/concurrent/t5.3.3.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <thread>
+#include <atomic>
+#include <assert.h>
+
+std::atomic<bool> x, y;
+std::atomic<int> z;
+
+void WriteX() {
+    x.store(true, std::memory_order_seq_cst);
+}
+
+void WriteY() {
+    y.store(true, std::memory_order_seq_cst);
+}
+
+void ReadXThenY() {
+    while (!x.load(std::memory_order_seq_cst)) {
+        // ignore
+    }
+    if (y.load(std::memory_order_seq_cst)) {
+        ++z;
+    }
+}
+
+void ReadYThenX() {
+    while (!y.load(std::memory_order_seq_cst)) {
+        // ignore
+    }
+    if (x.load(std::memory_order_seq_cst)) {
+        ++z;
+    }
+}
+int main() {
+    x = false;
+    y = false;
+    z = 0;
+    std::thread a(WriteX);
+    std::thread b(WriteY);
+    std::thread c(ReadXThenY);
+    std::thread d(ReadYThenX);
+    a.join();
+    b.join();
+    c.join();
+    d.join();
+    assert(z.load() != 0);
+    return 0;
+}
