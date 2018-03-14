2c76d2a4432a47d3741cdc9732cb86a35e42a23c
whutaihejin <taihejin@163.com>
2018-03-14 17:40:24 +0800

    forward

--- include/util.h
+++ include/util.h
@@ -1,7 +1,17 @@
 #ifndef UTIL_H
 #define UTIL_H
 
+#include <iostream>
+#include <vector>
+
 // print all elements of array
 #define PrintArray(x) for(size_t i = 0; i < (sizeof(x) / sizeof(x[0])); ++i) { std::cout << x[i] << " "; } std::cout << std::endl;
+
+inline void Print(const std::vector<int>& vec) {
+    for (auto it = vec.begin(); it != vec.end(); ++it) {
+        std::cout << *it << " ";
+    }
+    std::cout << std::endl;
+}
 
 #endif
---
+++ src/clrs/e2.2-2_select_sort.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include "util.h"
+
+void SelectSort(int a[], size_t size) {
+    for (size_t i = 0; i < size - 1; ++i) {
+        int min = i;
+        for (size_t j = i + 1; j < size; ++j) {
+            if (a[j] < a[min]) {
+                min = j;
+            }
+        }
+        int tmp = a[i];
+        a[i] = a[min];
+        a[min] = tmp;
+    }
+}
+int main() {
+    std::vector<int> vec{5, 4, 3, 2, 1};
+    SelectSort(&vec[0], vec.size());
+    Print(vec);
+    std::vector<int> vec2{5, 4, 8, 9, 3, 2, 1};
+    SelectSort(&vec2[0], vec2.size());
+    Print(vec2);
+    return 0;
+}
