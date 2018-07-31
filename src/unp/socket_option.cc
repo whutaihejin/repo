167a532c9572f64726685006a2ba25364c9972a6
whutaihejin <taihejin@163.com>
2018-07-31 14:11:33 +0800

    forward

--- src/unp/socket_option.cc
+++ src/unp/socket_option.cc
@@ -15,7 +15,25 @@ void PrintSocketFlag(const char* name, union SocketOptionEntry* entry, int len) 
     if (len != sizeof(int)) {
         printf("%s: size %d not sizeof(int) %d\n", name,  len, static_cast<int>(sizeof(int)));
     } else {
-        printf("%s: default=%s\n", name,  (entry->int_val == 0 ? "off" : "on"));
+        printf("%s: default=%d\n", name, entry->int_val);
+    }
+}
+
+void PrintSocketLinger(const char* name, union SocketOptionEntry* entry, int len) {
+    if (len == sizeof(struct linger)) {
+        struct linger& l = entry->linger_val;
+        printf("%s: linger{l_onoff=%d, l_linger=%d}\n", name, l.l_onoff, l.l_linger);
+    } else {
+        printf("%s: print error\n", name);
+    }
+}
+
+void PrintSocketTimeval(const char* name, union SocketOptionEntry* entry, int len) {
+    if (len == sizeof(struct timeval)) {
+        struct timeval& t = entry->timeval_val;
+        printf("%s: timeval{tv_sec=%ld, tv_usec=%d}\n", name, t.tv_sec, t.tv_usec);
+    } else {
+        printf("%s: print error\n", name);
     }
 }
 
@@ -27,57 +45,44 @@ struct SocketOption {
     void (*printer)(const char* name, union SocketOptionEntry*, int);
 };
 
-class Socket {
-public:
-    Socket(int family = -1, int type = -1): fd_(-1) {
-        if (family != -1) {
-            fd_ = socket(family, type, 0);
-        }
-        std::cout << "build Socket fd:" << fd_ << std::endl;
-    }
-
-    int GetDescriptor() {
-        return fd_;
-    }
-
-    ~Socket() {
-        if (fd_ != -1) {
-            close(fd_);
-        }
-    }
-private:
-    int fd_;
-};
-
 struct SocketOption socket_option_list[] = {
-    {"SO_BROADCAST", SOL_SOCKET, SO_BROADCAST, PrintSocketFlag}
+    {"SO_BROADCAST", SOL_SOCKET, SO_BROADCAST, PrintSocketFlag},
+    {"SO_DEBUG", SOL_SOCKET, SO_DEBUG, PrintSocketFlag},
+    {"SO_DONTROUTE", SOL_SOCKET, SO_DONTROUTE, PrintSocketFlag},
+    {"SO_ERROR", SOL_SOCKET, SO_ERROR, PrintSocketFlag},
+    {"SO_KEEPALIVE", SOL_SOCKET, SO_KEEPALIVE, PrintSocketFlag},
+    {"SO_LINGER", SOL_SOCKET, SO_LINGER, PrintSocketLinger},
+    {"SO_OOBINLINE", SOL_SOCKET, SO_OOBINLINE, PrintSocketFlag},
+    {"SO_RCVBUF", SOL_SOCKET, SO_RCVBUF, PrintSocketFlag},
+    {"SO_SNDBUF", SOL_SOCKET, SO_SNDBUF, PrintSocketFlag},
+    {"SO_RCVLOWAT", SOL_SOCKET, SO_RCVLOWAT, PrintSocketFlag},
+    {"SO_SNDLOWAT", SOL_SOCKET, SO_SNDLOWAT, PrintSocketFlag},
+    {"SO_RCVTIMEO", SOL_SOCKET, SO_RCVTIMEO, PrintSocketTimeval},
+    {"SO_SNDTIMEO", SOL_SOCKET, SO_SNDTIMEO, PrintSocketTimeval},
+    {"SO_REUSEADDR", SOL_SOCKET, SO_REUSEADDR, PrintSocketFlag},
+    {"SO_REUSEPORT", SOL_SOCKET, SO_REUSEPORT, PrintSocketFlag},
+    {"SO_TYPE", SOL_SOCKET, SO_TYPE, PrintSocketFlag},
+    {"SO_USELOOPBACK", SOL_SOCKET, SO_USELOOPBACK, PrintSocketFlag},
+    {NULL, 0, 0, NULL}
 };
 
 int main() {
-    Socket socket(AF_INET, SOCK_STREAM);
-    int flag = 0;
-    socklen_t len = sizeof(flag);
-    getsockopt(socket.GetDescriptor(), SOL_SOCKET, SO_DEBUG, static_cast<void*>(&flag), &len);
-    std::cout << flag << std::endl;
     union SocketOptionEntry entry;
     for (struct SocketOption* p = socket_option_list; p->opt_str != NULL; ++p) {
-        printf("%s: ", p->opt_str);
         if (p->printer == NULL) {
             printf("undefined\n");
             continue;
         }
-        Socket socket;
+        int fd = -1;
         switch (p->opt_level) {
             case SOL_SOCKET:
-                socket = Socket(AF_INET,  SOCK_STREAM);
+                fd = socket(AF_INET, SOCK_STREAM, 0);
                 break;
             default:
                 printf("can't create fd");
                 continue;
         }
         socklen_t len = sizeof(SocketOptionEntry);
-        int fd = socket.GetDescriptor();
-        std::cout << "fd = " << fd << std::endl;
         if (getsockopt(fd, p->opt_level, p->opt_name, &entry, &len) == -1) {
             std::cerr << " getsockopt error errno " << errno << " errmsg " << strerror(errno) << std::endl;
         } else {
