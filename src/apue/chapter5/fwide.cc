d578cfeac0fbf737b068741907bf21a8f24bc59a
whutaihejin <taihejin@163.com>
2018-09-09 23:09:17 +0800

    forward

---
+++ src/apue/chapter5/fwide.cc
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <wchar.h>
+#include <errno.h>
+#include <string.h>
+
+int main() {
+    // coredump code
+    // {
+    //     errno = 0;
+    //     int ret = fwide(NULL, -1);
+    //     printf("errno: %d msg: %s", errno, strerror(errno));
+    // }
+    // {
+    //     errno = 0;
+    //     int ret = fwide(NULL, 0);
+    //     printf("errno: %d msg: %s", errno, strerror(errno));
+    // }
+    // {
+    //     errno = 0;
+    //     int ret = fwide(NULL, 1);
+    //     printf("errno: %d msg: %s", errno, strerror(errno));
+    // }
+    {
+        errno = 0;
+        int ret = fwide(stdin, -1);
+        printf("ret: %d errno: %d msg: %s\n", ret, errno, strerror(errno));
+        // 
+        errno = 0;
+        ret = fwide(stdin, 0);
+        printf("ret: %d errno: %d msg: %s\n", ret, errno, strerror(errno));
+        // 
+        errno = 0;
+        ret = fwide(stdin, 1);
+        printf("ret: %d errno: %d msg: %s\n", ret, errno, strerror(errno));
+    }
+    {
+        errno = 0;
+        int ret = fwide(stdout, -1);
+        printf("ret: %d errno: %d msg: %s\n", ret, errno, strerror(errno));
+        // 
+        errno = 0;
+        ret = fwide(stdout, 0);
+        printf("ret: %d errno: %d msg: %s\n", ret, errno, strerror(errno));
+        // 
+        errno = 0;
+        ret = fwide(stdout, 1);
+        printf("ret: %d errno: %d msg: %s\n", ret, errno, strerror(errno));
+    }
+    {
+        errno = 0;
+        int ret = fwide(stderr, -1);
+        printf("ret: %d errno: %d msg: %s\n", ret, errno, strerror(errno));
+        // 
+        errno = 0;
+        int ret = fwide(stderr, 0);
+        printf("ret: %d errno: %d msg: %s\n", ret, errno, strerror(errno));
+        // 
+        errno = 0;
+        ret = fwide(stderr, 1);
+        printf("ret: %d errno: %d msg: %s\n", ret, errno, strerror(errno));
+    }
+}
