4e4532b23af0a4fc710b87e401c1e100348395da
zhanglonghanks <zhanglonghanks@didichuxing.com>
2019-05-24 11:43:09 +0800

    forward

---
+++ src/endeaver/gcd.cc
@@ -0,0 +1,28 @@
+// Copyright (c) 2018 The Didi Corporation and The Pickup Authors.
+// All rights reserved. Use of this source code should be governed
+// by the corporation. See the AUTHORS file for names of authors.
+// Author: zhanglonghanks              Email: taihejin@whu.edu.cn
+// Create: 2019-05-24 11:37:00 Last-Modified: 2019-05-24 11:37:00
+
+#include <iostream>
+
+int gcd(int a, int b) {
+    if (a < b) {
+        return gcd(b, a);
+    }
+    int r = 0;
+    while ((r = a % b)) {
+        a = b;
+        b = r;
+    }
+    return b;
+}
+
+int main() {
+    std::cout << "Enter two number:" << std::endl;
+    int a = 0, b = 0;
+    while (std::cin >> a >> b) {
+        std::cout << "gcd(" << a << ", " << b << ") =" << gcd(a, b) << std::endl;
+    }
+    return 0;
+}
