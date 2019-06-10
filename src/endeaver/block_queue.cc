f7dd6e1bba55a0bd7440c671dccb3506cdb5f275
whutaihejin <taihejin@163.com>
2019-06-10 11:47:56 +0800

    forward

--- src/endeaver/block_queue.cc
+++ src/endeaver/block_queue.cc
@@ -1,30 +1,76 @@
 #include <iostream>
 #include <pthread.h>
+#include <deque>
 
 class BlockingQueue {
 public:
-    BlockingQueue(): exit_(false) {}
-
-    bool Exit() {
-        return exit_;
+    BlockingQueue(const size_t capacity = 10): capacity_(capacity) {
+        pthread_mutex_init(&mutex_, NULL);
+        pthread_cond_init(&nonempty_cond_, NULL);
+        pthread_cond_init(&nonfull_cond_, NULL);
     }
-    void SetExit(bool val) {
-        exit_ = val;
+    ~BlockingQueue() {
+        pthread_mutex_destroy(&mutex_);
+        pthread_cond_destroy(&nonempty_cond_);
+        pthread_cond_destroy(&nonfull_cond_);
+    }
+
+    void Push(int entry) {
+        pthread_mutex_lock(&mutex_);
+        while (queue_.size() > capacity_) {
+            pthread_cond_wait(&nonfull_cond_, &mutex_);
+        }
+        queue_.push_back(entry);
+        // send non empty signal
+        pthread_cond_signal(&nonempty_cond_);
+        pthread_mutex_unlock(&mutex_);
+    }
+
+    int Pop() {
+        pthread_mutex_lock(&mutex_);
+        while (queue_.empty()) {
+            pthread_cond_wait(&nonempty_cond_, &mutex_);
+        }
+        int val = queue_.front();
+        queue_.pop_front();
+        pthread_cond_signal(&nonfull_cond_);
+        pthread_mutex_unlock(&mutex_);
+        return val;
     }
 
 private:
-    std::atomic<bool> exit_;
+    size_t capacity_;
+    std::deque<int> queue_;
+    pthread_mutex_t mutex_;
+    pthread_cond_t nonempty_cond_;
+    pthread_cond_t nonfull_cond_;
 };
 
 
 // global mutex for std::cout
 pthread_mutex_t cout_mutex = PTHREAD_MUTEX_INITIALIZER;
+struct TaskMeta {
+    TaskMeta(): exit(false), queue(NULL) {}
+    bool Exit() {
+        return exit;
+    }
+
+    void SetExit(bool val) {
+        exit = val;
+    }
+
+    std::atomic<bool> exit;
+    BlockingQueue* queue;
+};
 
 void* Task(void* args) {
-    BlockingQueue* queue = static_cast<BlockingQueue*>(args);
-    while (!queue->Exit()) {
+    TaskMeta* meta = static_cast<TaskMeta*>(args);
+    BlockingQueue* queue = meta->queue;
+    // consumer task
+    while (!meta->Exit()) {
+        int val = queue->Pop();
         pthread_mutex_lock(&cout_mutex);
-        std::cout << "take an element" << std::endl;
+        std::cout << "take an element " << val << std::endl;
         pthread_mutex_unlock(&cout_mutex);
     }
     pthread_mutex_lock(&cout_mutex);
@@ -35,17 +81,24 @@ void* Task(void* args) {
 
 int main() {
     BlockingQueue queue;
+    TaskMeta task_meta;
+    task_meta.queue = &queue;
+
     pthread_t tid;
-    if (pthread_create(&tid, NULL, Task, static_cast<void*>(&queue)) != 0) {
+    if (pthread_create(&tid, NULL, Task, static_cast<void*>(&task_meta)) != 0) {
         std::cout << "create pthread failed!" << std::endl;
         return -1;
     }
-    for (int i = 0; i < 10; ++i) {
-        pthread_mutex_lock(&cout_mutex);
-        std::cout << "push queue " << (i + 1) << std::endl;
-        pthread_mutex_unlock(&cout_mutex);
-    }
-    queue.SetExit(true);
+    // producer task
+    // for (int i = 0; i < 1000; ++i) {
+    //     queue.Push(i + 1);
+    //     pthread_mutex_lock(&cout_mutex);
+    //     std::cout << "push queue " << (i + 1) << std::endl;
+    //     pthread_mutex_unlock(&cout_mutex);
+    // }
+    size_t i = 0;
+    while (true) { queue.Push(i++); }
+    task_meta.SetExit(true);
     pthread_join(tid, NULL);
     std::cout << "join done, exit the main thread!" << std::endl;
 }
