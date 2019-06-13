42a5df08454f5e618b54077d6606af212cd8f8c4
whutaihejin <taihejin@163.com>
2019-06-13 17:08:16 +0800

    fix code

--- src/clrs/chapter2/binary_add.cc
+++ src/clrs/chapter2/binary_add.cc
@@ -4,70 +4,37 @@
 
 int BinaryAdd(std::vector<int>& a, std::vector<int>& b, std::vector<int>& s) {
     s.resize(a.size() + 1);
-    int carry = 0;
-    for (size_t i = 0; i < a.size(); ++i) {
-        s[i] = (a[i] + b[i] + carry) & 0x01;
+    int carry = 0, i = a.size() - 1;
+    for (; i >= 0; i--) {
+        s[i + 1] = (a[i] + b[i] + carry) & 0x01;
         carry = (a[i] + b[i] + carry) >> 1;
     }
-    s[a.size()] = carry;
+    s[0] = carry;
     return 0;
 }
 
-void Show(std::vector<int>& nums) {
+void Show(std::vector<int>& nums, std::vector<int>* next = NULL, std::vector<int>* sum = NULL) {
     std::for_each(nums.begin(), nums.end(), [](const int& v) { std::cout << v; });
+    if (next) {
+        std::cout << " + ";
+        std::for_each(next->begin(), next->end(), [](const int& v) { std::cout << v; });
+    }
+    if (sum) {
+        std::cout << " = ";
+        std::for_each(sum->begin(), sum->end(), [](const int& v) { std::cout << v; });
+    }
     std::cout << std::endl;
 }
 
 int main() {
-    {
-        std::vector<int> a{0, 0};
-        std::vector<int> b{0, 0};
-        std::vector<int> c;
-        BinaryAdd(a, b, c);
-        Show(a);
-        Show(b);
-        std::cout << "sum:";
-        Show(c);
-    }
-    {
-        std::vector<int> a{0, 1};
-        std::vector<int> b{0, 0};
-        std::vector<int> c;
-        BinaryAdd(a, b, c);
-        Show(a);
-        Show(b);
-        std::cout << "sum:";
-        Show(c);
-    }
-    {
-        std::vector<int> a{1, 0};
-        std::vector<int> b{0, 0};
-        std::vector<int> c;
-        BinaryAdd(a, b, c);
-        Show(a);
-        Show(b);
-        std::cout << "sum:";
-        Show(c);
-    }
-    {
-        std::vector<int> a{1, 1};
-        std::vector<int> b{0, 0};
-        std::vector<int> c;
-        BinaryAdd(a, b, c);
-        Show(a);
-        Show(b);
-        std::cout << "sum:";
-        Show(c);
-    }
-    {
-        std::vector<int> a{1, 1};
-        std::vector<int> b{1, 1};
-        std::vector<int> c;
-        BinaryAdd(a, b, c);
-        Show(a);
-        Show(b);
-        std::cout << "sum:";
-        Show(c);
+    for (int i = 0; i <= 3; i++) {
+        for (int j = 0; j <= 3; j++) {
+            std::vector<int> a{(i & 0x02) >> 1, i & 0x01};
+            std::vector<int> b{(j & 0x02) >> 1, j & 0x01};
+            std::vector<int> c;
+            BinaryAdd(a, b, c);
+            Show(a, &b, &c);
+        }
     }
     return 0;
 }
