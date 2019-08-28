5a225d3bca9cba8450c6d19be1d6a92fdf6bcd2f
whutaihejin <taihejin@163.com>
2019-08-28 10:08:55 +0800

    forward

--- src/apue/chapter8/fork_with_thread.cc
+++ src/apue/chapter8/fork_with_thread.cc
@@ -21,19 +21,11 @@ void* RunTask2(void *arg) {
 
 int main() {
     pthread_t tid, tid2;
-    pthread_create(&tid, nullptr, RunTask, nullptr);
+    // common thread
     pthread_create(&tid2, nullptr, RunTask2, nullptr);
-    // pthread_create((pthread_t*)&tids[1], nullptr, RunTask, &ids[1]);
-    // pthread_create((pthread_t*)&tids[2], nullptr, RunTask, &ids[2]);
-    // if (fork() == 0) {
-    //     // do nothing
-    //     printf("I am child process with %ld\n", (long)getpid());
-    //     sleep(3);
-    //     _exit(0);
-    // } else {
-    //     printf("I am parent process with %ld\n", (long)getpid());
-    //     sleep(1);
-    // }
+    // fork within this thread
+    pthread_create(&tid, nullptr, RunTask, nullptr);
+    // parent show
     printf("I am parent process with %ld\n", (long)getpid());
     sleep(3);
     return 0;
