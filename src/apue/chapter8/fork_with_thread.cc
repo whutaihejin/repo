22e82333d2eec5a9a999a8f0dc0d67a3b4842331
whutaihejin <taihejin@163.com>
2019-08-28 09:39:42 +0800

    forward

--- src/apue/chapter8/fork_with_thread.cc
+++ src/apue/chapter8/fork_with_thread.cc
@@ -8,7 +8,7 @@
 std::atomic<bool> stop(false);
 
 void* RunTask(void *arg) {
-    while (!stop.load(std::memory_order_acquire)) {
+    for (int i = 0; i < 3; ++i) {
         printf("pthread task %ld, %ld\n", (long)getpid(), (long)getppid());
     }
     return nullptr;
@@ -19,13 +19,15 @@ int main() {
     std::vector<pthread_t> tids[3];
     pthread_create((pthread_t*)&tids[0], nullptr, RunTask, &ids[0]);
     // pthread_create((pthread_t*)&tids[1], nullptr, RunTask, &ids[1]);
-    // pthread_create((pthread_t*)&tids[2], nullptr, RunTask, &ids[2]);
+    pthread_create((pthread_t*)&tids[2], nullptr, RunTask, &ids[2]);
     if (fork() == 0) {
         // do nothing
+        printf("I am child process with %ld\n", (long)getpid());
+        sleep(3);
+        _exit(0);
     } else {
+        printf("I am parent process with %ld\n", (long)getpid());
         sleep(1);
-        stop.store(true, std::memory_order_release);
-        printf("parent exit with %ld\n", (long)getpid());
     }
     return 0;
 }
