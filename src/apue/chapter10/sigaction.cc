77d91af7838219eaf7b50e095bb69a9b22516c4f
whutaihejin <taihejin@163.com>
2019-09-02 22:31:43 +0800

    forward

---
+++ src/apue/chapter10/sigaction.cc
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <string.h>
+#include <unistd.h>
+
+void sigQuit(int signo) {
+    printf("caught signo %s\n", strsignal(signo));
+}
+
+int main() {
+    struct sigaction act, oact;
+    sigset_t mask;
+    sigemptyset(&mask);
+    act.sa_mask = mask;
+    act.sa_flags = 0;
+#if defined(__APPLE__)
+    printf("compile on mac os x platform\n");
+    act.__sigaction_u.__sa_handler = sigQuit;
+#elif defined(__linux__)
+    printf("compile on linux platform\n");
+#else
+#error "unsuported platform"
+#endif
+    sigaction(SIGQUIT, &act, &oact);
+    for (;;) {
+        pause();
+    }
+    return 0;
+}
