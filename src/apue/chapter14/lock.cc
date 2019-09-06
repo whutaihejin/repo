9db24309b808a59c50a6a6004920f36f8cb864cd
whutaihejin <taihejin@163.com>
2019-09-06 17:31:55 +0800

    forward

--- src/apue/chapter14/lock.cc
+++ src/apue/chapter14/lock.cc
@@ -3,51 +3,75 @@
 #include <unistd.h>
 #include <fcntl.h>
 
+int LockFile(int fd, int cmd, int type, int whence, off_t offset, off_t len) {
+    struct flock l;
+    l.l_type = type;
+    l.l_whence = whence;
+    l.l_start = offset;
+    l.l_len = len;
+    return fcntl(fd, cmd, &l);
+}
+
+int ReadLockFile(int fd, int whence, off_t offset, off_t len) {
+    return LockFile(fd, F_SETLK, F_RDLCK, whence, offset, len);
+}
+
+int ReadwLockFile(int fd, int whence, off_t offset, off_t len) {
+    return LockFile(fd, F_SETLKW, F_RDLCK, whence, offset, len);
+}
+
+int WriteLockFile(int fd, int whence, off_t offset, off_t len) {
+    return LockFile(fd, F_SETLK, F_WRLCK, whence, offset, len);
+}
+
+int WritewLockFile(int fd, int whence, off_t offset, off_t len) {
+    return LockFile(fd, F_SETLKW, F_WRLCK, whence, offset, len);
+}
+
+int UnlockFile(int fd, int whence, off_t offset, off_t len) {
+    return LockFile(fd, F_SETLKW, F_UNLCK, whence, offset, len);
+}
+
+int TestFileLock(int fd, int type, int whence, off_t offset, off_t len) {
+    struct flock l;
+    l.l_type = type;
+    l.l_whence = whence;
+    l.l_start = offset;
+    l.l_len = len;
+    if (fcntl(fd, F_GETLK, &l) != 0) {
+        return -1;
+    }
+    return (l.l_type == F_UNLCK) ? 0 : l.l_pid;
+}
+
 int main() {
     const char* filename = ".lock.file";
     pid_t pid;
     if ((pid = fork()) == 0) {
         sleep(1);
         printf("child pid = %ld\n", (long)getpid());
-
         int fd = open(filename, O_RDWR | O_CREAT, S_IRWXU);
 
-        for (int i = 0; i < 5; ++i) {
-            struct flock flock;
-            flock.l_type = F_WRLCK;
-            flock.l_whence = SEEK_SET;
-            flock.l_start = 0;
-            flock.l_len = 0;
-            int ret = fcntl(fd, F_GETLK, &flock);
-            if (flock.l_type == F_UNLCK) {
-                printf("child ret=%d no locked\n", ret);
-            } else {
-                printf("child ret=%d locked by pid=%lu\n", ret, (long)flock.l_pid);
-            }
+        for (int i = 0; i < 6; ++i) {
+            int ret = TestFileLock(fd, F_WRLCK, SEEK_SET, 0, 0);
+            printf("child locked by pid=%d\n", ret);
             sleep(1);
         }
-
-
 
         exit(0);
     }
     //
     printf("parent pid=%ld\n", (long)getpid());
     int fd = open(filename, O_RDWR | O_CREAT, S_IRWXU);
-    struct flock flock;
-    flock.l_type = F_WRLCK;
-    flock.l_whence = SEEK_SET;
-    flock.l_start = 0;
-    flock.l_len = 0;
-    int ret = fcntl(fd, F_SETLK, &flock);
-    printf("ret=%d\n", ret);
+    int ret = WriteLockFile(fd, SEEK_SET, 0, 0);
+    printf("parent lock file ret=%d\n", ret);
     //
     sleep(3);
-    // close(fd);
-    flock.l_type = F_UNLCK;
-    ret = fcntl(fd, F_SETLK, &flock);
-    printf("unlock ret=%d\n", ret);
+    close(fd);
+    // ret = UnlockFile(fd, SEEK_SET, 0, 0);
+    printf("parent unlock file ret=%d\n", 0);
 
     sleep(8);
+    unlink(filename);
     return 0;
 }
