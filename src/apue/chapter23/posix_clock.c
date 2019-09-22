cf78d2a09a1e5ffda4f10b17f483eb573f27e752
taihejin <taihejin2008@126.com>
2019-09-22 16:18:11 +0800

    forward

--- src/apue/chapter23/posix_clock.c
+++ src/apue/chapter23/posix_clock.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <time.h>
+#include <unistd.h>
+#include <pthread.h>
 
 int main() {
     struct timespec spec;
@@ -10,6 +12,29 @@ int main() {
     }
     if (clock_getres(CLOCK_REALTIME, &res) == 0) {
         printf("res: %2ld.%09ld\n", res.tv_sec, res.tv_nsec);
+    }
+   
+    // CLOCK_MONOTONIC
+    if (clock_gettime(CLOCK_MONOTONIC, &spec) == 0) {
+        printf("MONOTONIC: %2ld.%09ld\n", spec.tv_sec, spec.tv_nsec);
+    }
+    if (clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &spec) == 0) {
+        printf("CLOCK_PROCESS_CPUTIME_ID: %2ld.%09ld\n", spec.tv_sec, spec.tv_nsec);
+    }
+    if (clock_gettime(CLOCK_THREAD_CPUTIME_ID, &spec) == 0) {
+        printf("CLOCK_THREAD_CPUTIME_ID: %2ld.%09ld\n", spec.tv_sec, spec.tv_nsec);
+    }
+
+    clockid_t clockid;
+    if (clock_getcpuclockid(getpid(), &clockid) == 0) {
+        if (clock_gettime(clockid, &spec) == 0) {
+            printf("process %lu: %2ld.%09ld\n", (long)clockid, spec.tv_sec, spec.tv_nsec);
+        }
+    }
+    if (pthread_getcpuclockid(pthread_self(), &clockid) == 0) {
+        if (clock_gettime(clockid, &spec) == 0) {
+            printf("pthread %lu: %2ld.%09ld\n", (long)clockid, spec.tv_sec, spec.tv_nsec);
+        }
     }
     return 0;
 }
