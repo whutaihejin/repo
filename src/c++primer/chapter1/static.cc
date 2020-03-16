b791ccba56c7bf7378459b89b4340cdef7402a57
qqlongzhang <qqlongzhang@tencent.com>
2020-03-16 10:26:03 +0800

    forward

---
+++ src/c++primer/chapter1/static.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int gen() {
+    std::cout << "gen -> " << 1 << std::endl;
+    return 1;
+}
+
+void f() {
+    static int x = gen();
+    std::cout << "f -> " << x << std::endl;
+}
+
+int main() {
+    f();
+    f();
+    return 0;
+}
