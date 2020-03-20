0e59d93ec1d989e584d85810aa143226ff1eaa2a
whutaihejin <taihejin@163.com>
2020-03-20 13:08:17 +0800

    forward

---
+++ src/primer/lc/1302_deepest_leaves_sum.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <deque>
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    int deepestLeavesSum(TreeNode* root) {
+        int val = 0, size = 1;
+        std::deque<TreeNode*> queue{root};
+        while (!queue.empty()) {
+            int cur = 0;
+            for (int i = 0; i < size; ++i) {
+                TreeNode* node = queue.front();
+                queue.pop_front();
+                if (node->left) queue.push_back(node->left);
+                if (node->right) queue.push_back(node->right);
+                cur += node->val;
+            }
+            val = cur; // current level' sum
+            size = queue.size();
+        }
+        return val;
+    }
+};
