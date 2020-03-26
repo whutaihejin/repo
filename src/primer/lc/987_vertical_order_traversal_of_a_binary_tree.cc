4fc0b3d5028b9077666e7732787d007369a60703
whutaihejin <taihejin@163.com>
2020-03-26 10:06:53 +0800

    vertical traversal

---
+++ src/primer/lc/987_vertical_order_traversal_of_a_binary_tree.cc
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <algorithm>
+
+using namespace std;
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    struct K {int y; TreeNode* n; };
+    static bool comp(struct K& x, struct K& y) {
+        if (x.y != y.y) return x.y > y.y;
+        return x.n->val < y.n->val;
+    }
+    vector<vector<int>> verticalTraversal(TreeNode* root) {
+        std::map<int, vector<K>> map;
+        vector<vector<int>> rst;
+        preOrder(root, 0, 0, map);
+        for (auto it = map.begin(); it != map.end(); ++it) {
+            auto& vec = it->second;
+            std::sort(vec.begin(), vec.end(), comp);
+            vector<int> e;
+            for (auto i = vec.begin(); i != vec.end(); ++i) {
+                e.push_back(i->n->val);
+            }
+            rst.push_back(e);
+        }
+        return rst;
+    }
+
+    void preOrder(TreeNode* root, int x, int y, std::map<int, vector<K>>& m) {
+        if (!root) return;
+        auto it = m.find(x);
+        if (it == m.end()) {
+            m[x] = vector<K>{{y, root}};
+        } else {
+            struct K k{y, root};
+            it->second.emplace_back(k);
+        }
+        preOrder(root->left, x - 1, y - 1, m);
+        preOrder(root->right, x + 1, y - 1, m);
+    }
+};
+
+int main() {
+    return 0;
+}
