543370f3d17dff0047958e3660889868986fc725
qqlongzhang <qqlongzhang@tencent.com>
2020-05-09 20:51:07 +0800

    fight dynamic programming

---
+++ src/primer/lc/120.triangle.cpp
@@ -0,0 +1,54 @@
+/*
+ * @lc app=leetcode id=120 lang=cpp
+ *
+ * [120] Triangle
+ *
+ * https://leetcode.com/problems/triangle/description/
+ *
+ * algorithms
+ * Medium (43.04%)
+ * Likes:    1749
+ * Dislikes: 210
+ * Total Accepted:    234.5K
+ * Total Submissions: 544.8K
+ * Testcase Example:  '[[2],[3,4],[6,5,7],[4,1,8,3]]'
+ *
+ * Given a triangle, find the minimum path sum from top to bottom. Each step
+ * you may move to adjacent numbers on the row below.
+ * 
+ * For example, given the following triangle
+ * 
+ * 
+ * [
+ * ⁠    [2],
+ * ⁠   [3,4],
+ * ⁠  [6,5,7],
+ * ⁠ [4,1,8,3]
+ * ]
+ * 
+ * 
+ * The minimum path sum from top to bottom is 11 (i.e., 2 + 3 + 5 + 1 = 11).
+ * 
+ * Note:
+ * 
+ * Bonus point if you are able to do this using only O(n) extra space, where n
+ * is the total number of rows in the triangle.
+ * 
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        if (triangle.empty() || triangle[0].empty()) return 0;
+        int m = triangle.size();
+        for (int i = m - 2; i >= 0; --i) {
+            for (int j = 0; j < triangle[i].size(); ++j) {
+                triangle[i][j] += min(triangle[i + 1][j], triangle[i + 1][j + 1]);
+            }
+        }
+        return triangle[0][0];
+    }
+};
+// @lc code=end
+
---
+++ src/primer/lc/64.minimum-path-sum.cpp
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode id=64 lang=cpp
+ *
+ * [64] Minimum Path Sum
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        if (grid.empty() || grid[0].empty()) return 0;
+        int m = grid.size(), n = grid[0].size();
+        for (int j = 1; j < n; ++j) {
+            grid[0][j] += grid[0][j - 1];
+        }
+        for (int i = 1; i < m; ++i) {
+            grid[i][0] += grid[i - 1][0];
+        }
+        for (int i = 1; i < m; ++i) {
+            for (int j = 1; j < n; ++j) {
+                grid[i][j] += min(grid[i - 1][j], grid[i][j - 1]);
+            }
+        }
+        return grid[m - 1][n - 1];
+    }
+};
+// @lc code=end
+
