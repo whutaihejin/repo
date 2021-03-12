74a61abb192ca0d4574ea92f9429e1bb240d8270
whutaihejin <taihejin@163.com>
2021-03-12 10:14:21 +0800

    fix

--- src/clrs/chapter1/seletion_sort.cc
+++ src/clrs/chapter1/seletion_sort.cc
@@ -16,10 +16,26 @@ void SelectionSort(std::vector<int>& nums) {
     }
 }
 
+void SelectionSortDesc(std::vector<int>& nums) {
+    int limit = nums.size();
+    for (int i = 0; i < limit; ++i) {
+        int m = i, high = nums[i];
+        for (int j = i + 1; j < limit; ++j) {
+            if (nums[j] > high) {
+                m = j;
+                high = nums[j]; // update low index and value
+            }
+        }
+        std::swap(nums[i], nums[m]);
+    }
+}
+
 int main() {
     std::vector<int> nums{5, 2, 4, 6, 1, 3};
     std::for_each(nums.begin(), nums.end(), [](const int& x) { std::cout << x << ", "; }); std::cout << std::endl;
     SelectionSort(nums);
+    std::for_each(nums.begin(), nums.end(), [](const int& x) { std::cout << x << ", "; }); std::cout << std::endl;
+    SelectionSortDesc(nums);
     std::for_each(nums.begin(), nums.end(), [](const int& x) { std::cout << x << ", "; }); std::cout << std::endl;
     return 0;
 }
