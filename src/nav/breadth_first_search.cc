095f86cf568cb8908d6154ce59524caae2bcbc2c
whutaihejin <taihejin@163.com>
2021-04-01 20:50:24 +0800

    bfs

--- src/nav/breadth_first_search.cc
+++ src/nav/breadth_first_search.cc
@@ -11,7 +11,7 @@ int main() {
 
   std::vector<unsigned> r = bfs(g);
   for (unsigned u = 0; u < r.size(); ++u) {
-    std::cout << u << " ";
+    std::cout << r[u] << " ";
   }
   std::cout << std::endl;
   return 0;
--- src/nav/graph.h
+++ src/nav/graph.h
@@ -4,6 +4,8 @@
 
 #include <iostream>
 #include <vector>
+#include <deque>
+
 #include "routingkit/all.h"
 
 namespace nav {
@@ -34,7 +36,58 @@ public:
   friend std::ostream& operator<<(std::ostream& os, const Graph& g);
 };
 
-  
+// inner breadth first search
+void bfs(Graph& g, unsigned s, std::vector<unsigned>& visit, std::vector<unsigned>& r) {
+  std::deque<unsigned> queue{s};
+  while (!queue.empty()) {
+    int u = queue.front();
+    r.push_back(u);
+    queue.pop_front();
+    for (unsigned uv = g.first_out[u]; uv < g.first_out[u + 1]; ++uv) {
+      unsigned v = g.head[uv];
+      if (visit[v] == 0) {
+        queue.push_back(v);
+        // mark already visited
+        visit[v] = 1;
+      }
+    }
+  }
+}
+
+// breadth first search
+std::vector<unsigned> bfs(Graph& g) {
+  std::vector<unsigned> r;
+  std::vector<unsigned> visit(g.node_count, 0);
+  for (unsigned u = 0; u < g.node_count; ++u) {
+    if (!visit[u]) {
+      bfs(g, u, visit, r);
+    }
+  }
+  return r;
+}
+
+// cal out degree
+std::vector<int> OutDegree(Graph& g) {
+  std::vector<int> out(g.node_count, 0);
+  for (unsigned i = 0; i < g.node_count; ++i) {
+    out[i] = g.first_out[i + 1] - g.first_out[i];
+  }
+  return out;
+}
+
+// cal in degree
+std::vector<int> InDegree(Graph& g) {
+  std::vector<int> in(g.node_count, 0);
+  for (unsigned u = 0; u < g.node_count; ++u) {
+    for (unsigned uv = g.first_out[u]; uv < g.first_out[u + 1]; ++uv) {
+      unsigned v = g.head[uv];
+      in[v]++;
+    }
+  }
+  return in;
+}
+
+// output stream
 std::ostream& operator<<(std::ostream& os, const Graph& g) {
     for (unsigned x = 0; x < g.node_count; ++x) {
       if (x) {
